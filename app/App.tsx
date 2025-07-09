811a8e17ababe5b238e0143cfc9a24e607c00242
AngKalinina <angelina.k.data@gmail.com>
2025-07-09 17:38:22 +0400

    stable build: vite + tailwind + postcss

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello from App!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ app/main.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Убедись, что App.tsx лежит в этой же папке
+
+import "./app.css"; // если используешь стили
+
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
