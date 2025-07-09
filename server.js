34349fe1c3a87025c8f08365de21d0bd9c877fd1
AngKalinina <angelina.k.data@gmail.com>
2025-07-09 18:31:28 +0400

    switch to Express server for Railway deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const port = process.env.PORT || 5173;
+
+// Отдаём все файлы из /dist
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Для SPA: возвращаем index.html на все маршруты
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server is running: http://localhost:${port}`);
+});
